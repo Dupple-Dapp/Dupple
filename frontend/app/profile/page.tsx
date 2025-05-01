f505734ed6a08958826bf7bbb708ecda7d586bf9
Kudaru Deborah Nshuk <deborahkudaru@gmail.com>
2025-05-01 07:34:14 +0100

    add profile, navigation, and header components with tab functionality

--- frontend/app/profile/page.tsx
+++ frontend/app/profile/page.tsx
@@ -1,17 +1,42 @@
-import { Settings, UserRoundPen } from "lucide-react";
+"use client";
 
-const page = () => {
+import React, { useState } from 'react';
+import { TabName } from '@/types';
+import Header from '@/components/Layout/Header';
+import NavigationBar from '@/components/Navigation/NavigationBar';
+import HomePage from '@/components/Pages/HomePage';
+import ExplorePage from '@/components/Pages/ExplorePage';
+import MessagesPage from '@/components/Pages/MessagePage';
+import ProfilePage from '@/components/Pages/ProfilePage';
+
+const Page: React.FC = () => {
+  const [activeTab, setActiveTab] = useState<TabName>('profile');
+
+  // Render the active page based on the selected tab
+  const renderPage = () => {
+    switch (activeTab) {
+      case 'home':
+        return <HomePage />;
+      case 'explore':
+        return <ExplorePage />;
+      case 'messages':
+        return <MessagesPage />;
+      case 'profile':
+        return <ProfilePage />;
+      default:
+        return <ProfilePage />;
+    }
+  };
+
   return (
-    <div className="p-5 bg-white text-gray-700">
-      <div className="flex justify-between">
-        <h1>Profile</h1>
-        <div className="flex gap-4">
-        <Settings />
-        <UserRoundPen />
-        </div>
+    <div className="bg-white text-gray-700 h-screen relative pb-16">
+      <Header activeTab={activeTab} />
+      <div className="overflow-y-auto h-full pb-16">
+        {renderPage()}
       </div>
+      <NavigationBar activeTab={activeTab} setActiveTab={setActiveTab} />
     </div>
   );
 };
 
-export default page;
+export default Page;
